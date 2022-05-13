4187a5010d86796e41b67ee9505671a6608c6239
cubeboy <cubeboy.kim@gmail.com>
2022-05-13 16:23:24 +0900

    create product composite service

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,10 +1,18 @@
-package se.magnus.microservices.composite.product.productcompositeservice;
+package se.magnus.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
+@ComponentScan("se.magnus")
 @SpringBootApplication
 public class ProductCompositeServiceApplication {
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
---
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -0,0 +1,134 @@
+package se.magnus.microservices.composite.product.services;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.product.ProductService;
+import se.magnus.api.core.recommendation.Recommendation;
+import se.magnus.api.core.recommendation.RecommendationService;
+import se.magnus.api.core.review.Review;
+import se.magnus.api.core.review.ReviewService;
+import se.magnus.util.exceptions.InvalidInputException;
+import se.magnus.util.exceptions.NotFoundException;
+import se.magnus.util.http.HttpErrorInfo;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Slf4j
+@Component
+public class ProductCompositeIntegration implements ProductService, RecommendationService, ReviewService {
+  private final RestTemplate restTemplate;
+  private final ObjectMapper mapper;
+
+  private final String productServiceUrl;
+  private final String recommendationServiceUrl;
+  private final String reviewServiceUrl;
+
+  @Autowired
+  public ProductCompositeIntegration(
+    RestTemplate restTemplate,
+        ObjectMapper mapper,
+
+        @Value("${app.product-service.host}") String productServiceHost,
+        @Value("${app.product-service.port}") int    productServicePort,
+
+        @Value("${app.recommendation-service.host}") String recommendationServiceHost,
+        @Value("${app.recommendation-service.port}") int    recommendationServicePort,
+
+        @Value("${app.review-service.host}") String reviewServiceHost,
+        @Value("${app.review-service.port}") int    reviewServicePort
+  ) {
+    this.restTemplate = restTemplate;
+    this.mapper = mapper;
+
+    productServiceUrl        = "http://" + productServiceHost + ":" + productServicePort + "/product/";
+    recommendationServiceUrl = "http://" + recommendationServiceHost + ":" + recommendationServicePort + "/recommendation?productId=";
+    reviewServiceUrl         = "http://" + reviewServiceHost + ":" + reviewServicePort + "/review?productId=";
+  }
+
+  private String getErrorMessage(HttpClientErrorException ex) {
+    try {
+        return mapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
+    } catch (IOException ioex) {
+        return ex.getMessage();
+    }
+}
+
+
+  @Override
+  public List<Review> getReviews(int productId) {
+    try {
+      String url = reviewServiceUrl + productId;
+
+      log.debug("Will call getReviews API on URL: {}", url);
+      List<Review> reviews = restTemplate.exchange(url, GET, null, new ParameterizedTypeReference<List<Review>>() {}).getBody();
+
+      log.debug("Found {} reviews for a product with id: {}", reviews.size(), productId);
+      return reviews;
+
+  } catch (Exception ex) {
+      log.warn("Got an exception while requesting reviews, return zero reviews: {}", ex.getMessage());
+      return new ArrayList<>();
+  }
+  }
+
+  @Override
+  public List<Recommendation> getRecommendations(int productId) {
+    try {
+      String url = recommendationServiceUrl + productId;
+
+      log.debug("Will call getRecommendations API on URL: {}", url);
+      List<Recommendation> recommendations = restTemplate.exchange(url, GET, null, new ParameterizedTypeReference<List<Recommendation>>() {}).getBody();
+
+      log.debug("Found {} recommendations for a product with id: {}", recommendations.size(), productId);
+      return recommendations;
+
+    } catch (Exception ex) {
+      log.warn("Got an exception while requesting recommendations, return zero recommendations: {}", ex.getMessage());
+      return new ArrayList<>();
+    }
+  }
+
+  @Override
+  public Product getProduct(int productId) {
+    
+    try {
+      String url = productServiceUrl + productId;
+      log.debug("Will call getProduct API on URL: {}", url);
+
+      Product product = restTemplate.getForObject(url, Product.class);
+      log.debug("Found a product with id: {}", product.getProductId());
+
+      return product;
+
+    } catch (HttpClientErrorException ex) {
+      switch (ex.getStatusCode()) {
+
+        case NOT_FOUND:
+            throw new NotFoundException(getErrorMessage(ex));
+
+        case UNPROCESSABLE_ENTITY :
+            throw new InvalidInputException(getErrorMessage(ex));
+
+        default:
+            log.warn("Got a unexpected HTTP error: {}, will rethrow it", ex.getStatusCode());
+            log.warn("Error body: {}", ex.getResponseBodyAsString());
+            throw ex;
+      }
+    }
+  }
+  
+}
